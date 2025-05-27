e780fecdc67af025fd5188999d814a13f9f2cd85
Thais Canguçu <tatacangucu@Thaiss-MacBook-Air.local>
2025-05-27 17:22:46 -0300

    Create ContentViewModel

---
+++ Tracker/ViewModel/ContentViewModel.swift
@@ -0,0 +1,19 @@
+import Foundation
+import CoreData
+
+class ContentViewModel: ObservableObject {
+    @Published var products: [Product] = []
+    
+    func getProduct() {
+        products = CoreDataController.shared.fetchAllProducts()
+    }
+    
+    func createProduct(id: UUID, name: String, priceBuy: Double, priceSell: Double, stock: Int64) {
+        let result = CoreDataController.shared.createProduct(id: id, name: name, priceBuy: priceBuy, priceSell: priceSell, stock: stock)
+        self.products.append(result)
+    }
+    
+    func deleteToDo(_ product: Product) {
+        CoreDataController.shared.deleteProduct(product)
+    }
+}
