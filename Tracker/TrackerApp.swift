f33479e38d8d3d9ed573661f19002b7b8738644b
Thais Canguçu <tatacangucu@Thaiss-MacBook-Air.local>
2025-05-27 16:19:27 -0300

    initial Commit

---
+++ Tracker/TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerApp.swift
+//  Tracker
+//
+//  Created by Thais Canguçu on 27/05/25.
+//
+
+import SwiftUI
+
+@main
+struct TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
