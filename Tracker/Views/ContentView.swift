d8137341c2e6d5200b1f99b29773be9b625317f2
anshu Li <anshuli@MacBook-Pro-35.local>
2025-06-06 14:45:19 -0300

    Changed tab bar color

--- Tracker/Views/ContentView.swift
+++ Tracker/Views/ContentView.swift
@@ -9,14 +9,20 @@ struct ContentView: View {
                 .tabItem{
                     Label("Dashboard", systemImage: "chart.xyaxis.line")
                 }
+                .toolbarBackground(.navy, for: .tabBar)
+                            .toolbarBackground(.visible, for: .tabBar)
             ProductStockView()
                 .tabItem{
                     Label("Stock", systemImage: "shippingbox")
                 }
+                .toolbarBackground(.navy, for: .tabBar)
+                            .toolbarBackground(.visible, for: .tabBar)
             CalendarView()
                 .tabItem{
                     Label("Calendar", systemImage: "calendar")
                 }
+                .toolbarBackground(.navy, for: .tabBar)
+                            .toolbarBackground(.visible, for: .tabBar)
             //            Sheets()
             //                .tabItem{
             //                    Label("Sheets", systemImage: "doc.text")
