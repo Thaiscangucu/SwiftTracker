4737fbae6780e52a2fb1f0ee5e77503d43a83ee7
João Vitor Rocha <81839737+J040VRM@users.noreply.github.com>
2025-06-09 20:57:38 -0300

    Fixed pieChart get data function

--- Tracker/Views/Charts/PieCharts/ProductSellPieChart.swift
+++ Tracker/Views/Charts/PieCharts/ProductSellPieChart.swift
@@ -1,44 +1,23 @@
-import SwiftUI
-import Charts
-
-//Erro ao adcionar uma nova venda, ele nao soma. e quando eu deleto a venda(back ele nao remove do grafico)
-
-//Testar primeiro transformando essa struct em classe
-
-struct PieProfitChart: Identifiable {
-    let id = UUID()
-    let title: String
-    let revenue: Double
-}
-
 struct ProductSellPieChart: View {
     @StateObject var viewModel: ContentViewModel
-    @State public var products: [PieProfitChart] = []
-    
+
+    var products: [PieProfitChart] {
+        return [
+            PieProfitChart(title: "Compras", revenue: viewModel.totalBuy),
+            PieProfitChart(title: "Vendido", revenue: viewModel.totalSell)
+        ]
+    }
+
     var body: some View {
         Chart(products) { product in
             SectorMark(
-                angle: .value(
-                    Text(verbatim: product.title),
-                    product.revenue
-                )
+                angle: .value(product.title, product.revenue)
             )
-            .foregroundStyle(
-                by: .value(
-                    Text(verbatim: product.title),
-                    product.title
-                )
-            )
+            .foregroundStyle(by: .value(product.title, product.title))
         }
         .onAppear {
             viewModel.updateProductValue(products: viewModel.products)
             viewModel.updateSellValue(sells: viewModel.sells)
-            
-            products.append(PieProfitChart(title: "Compras", revenue: viewModel.totalBuy))
-            products.append(PieProfitChart(title: "Vendido", revenue: viewModel.totalSell))
-            
         }
-        
     }
-        
 }
