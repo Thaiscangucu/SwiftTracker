92eecb1c5a26823ce18dbde22f57505fb4ecec42
Thais Cangucu <thaiscangucu@Thaiss-MacBook-Pro.local>
2025-06-10 14:23:01 -0300

    Merge Errors

---
+++ Tracker/SellsWidget/SellsWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  SellsWidgetBundle.swift
+//  SellsWidget
+//
+//  Created by Thais Cangucu on 09/06/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct SellsWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        SellsWidget()
+    }
+}
