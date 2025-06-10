dbbf14b4a192a582c76904b0c748aac4b920aec4
Thais Cangucu <thaiscangucu@Thaiss-MacBook-Pro.local>
2025-06-10 16:12:27 -0300

    created widgets

---
+++ Widgets/WidgetsBundle.swift
@@ -0,0 +1,16 @@
+//
+//  WidgetsBundle.swift
+//  Widgets
+//
+//  Created by Thais Cangucu on 10/06/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        SellsWidget()
+    }
+}
