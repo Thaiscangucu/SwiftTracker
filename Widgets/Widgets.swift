9f90b7158c6b045cd96bbc72f2ce709abb741514
Thais Cangucu <thaiscangucu@Thaiss-MacBook-Pro.local>
2025-06-10 14:43:02 -0300

    widget new scheme

---
+++ Widgets/Widgets.swift
@@ -0,0 +1,93 @@
+//
+//  Widgets.swift
+//  Widgets
+//
+//  Created by Thais Cangucu on 10/06/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date(), emoji: "😀")
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date(), emoji: "😀")
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate, emoji: "😀")
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+
+//    func relevances() async -> WidgetRelevances<Void> {
+//        // Generate a list containing the contexts this widget is relevant in.
+//    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+    let emoji: String
+}
+
+struct WidgetsEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        VStack{
+            // TODO: Get real sells value in text
+            Text("34")
+                .font(.system(size: 70, weight: .bold))
+                .foregroundStyle(.texto)
+                
+            Text("Vendas")
+                .font(.title)
+                .fontWeight(.bold)
+                .minimumScaleFactor(0.6)
+                            .foregroundStyle(.texto)
+        }
+        .padding()
+    }
+}
+
+struct Widgets: Widget {
+    let kind: String = "Widgets"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            if #available(iOS 17.0, *) {
+                WidgetsEntryView(entry: entry)
+                    .containerBackground(for: .widget){
+                        LinearGradient(colors: [Color.backgroundGradient, Color.widgetBackground], startPoint: .bottom, endPoint: .top)
+                    }
+                
+            } else {
+                WidgetsEntryView(entry: entry)
+                    .padding()
+                    .background()
+            }
+        }
+        .configurationDisplayName("My Widget")
+        .description("This is an example widget.")
+    }
+}
+
+#Preview(as: .systemSmall) {
+    Widgets()
+} timeline: {
+    SimpleEntry(date: .now, emoji: "😀")
+    SimpleEntry(date: .now, emoji: "🤩")
+}
